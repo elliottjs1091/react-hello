deb3738051f2eb73374aee507174fe89615052a4
James <elliottjs1091@gmail.com>
2023-09-07 21:35:31 +1200

    react page

---
+++ src/MyApp.js
@@ -0,0 +1,17 @@
+function MyButton() {
+    return (
+      <button>
+        I'll be reacting!
+      </button>
+    );
+  }
+  
+export default function MyApp() {
+    return (
+        <div>
+        <h1>Hello, React.</h1>
+        <MyButton />
+        </div>
+    );
+}
+  
